ed423e6a1faca8ec543923dbc9ca633ef7976e1c
libertyernie <maybeway36@gmail.com>
2015-08-12 18:00:34 -0500

    Import exportloop from old branch

---
+++ ExportLoopVB/Form1.vb
@@ -0,0 +1,245 @@
+﻿Imports System.IO
+Imports System.Text
+
+Public Class Form1
+    Private currentProcess As Process
+
+    ''' <summary>
+    ''' Quote pathnames in arguments so other Windows applications will understand them.
+    ''' Converted from https://github.com/gfxmonk/csharp-quote-argv/blob/master/QuoteArguments.cs
+    ''' </summary>
+    ''' <param name="arg">An argument to escape</param>
+    ''' <returns>The escaped argument</returns>
+    Private Shared Function QuoteArgument(arg As String) As String
+        Dim backslashes As Integer = 0
+
+        Dim sb As New StringBuilder
+
+        Dim needquote As Boolean = arg.Length = 0 Or arg.Contains(" ") Or arg.Contains(vbTab)
+        If needquote Then
+            sb.Append(""""c)
+        End If
+
+        For Each c As Char In arg
+            If c = "\"c Then
+                backslashes += 1
+            ElseIf c = """"c Then
+                ' Make sure pending backslashes are doubled
+                sb.Append(New String("\"c, backslashes * 2))
+                backslashes = 0
+
+                ' Append a backslash-escaped double quote
+                sb.Append("\""")
+            Else
+                If backslashes > 0 Then
+                    sb.Append(New String("\"c, backslashes))
+                    backslashes = 0
+                End If
+
+                sb.Append(c)
+            End If
+        Next
+
+        If backslashes > 0 Then
+            sb.Append(New String("\"c, backslashes))
+        End If
+
+        If needquote Then
+            sb.Append(New String("\"c, backslashes))
+            sb.Append(""""c)
+        End If
+
+        Return sb.ToString()
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtOutputDir.Text = Environment.CurrentDirectory
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        If OpenFileDialog1.ShowDialog = DialogResult.OK Then
+            For Each f In OpenFileDialog1.FileNames
+                ListView1.Items.Add(f)
+            Next
+        End If
+    End Sub
+
+    Private Sub btnRemove_Click(sender As Object, e As EventArgs) Handles btnRemove.Click
+        For Each item As ListViewItem In ListView1.SelectedItems
+            ListView1.Items.Remove(item)
+        Next
+    End Sub
+
+    Private Sub ListView1_DragEnter(sender As Object, e As DragEventArgs) Handles ListView1.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Link
+        End If
+    End Sub
+
+    Private Sub ListView1_DragDrop(sender As Object, e As DragEventArgs) Handles ListView1.DragDrop
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            Dim filenames As String() = e.Data.GetData(DataFormats.FileDrop)
+            For Each filename As String In filenames
+                ListView1.Items.Add(filename)
+            Next
+        End If
+    End Sub
+
+    Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        If Not String.IsNullOrEmpty(txtOutputDir.Text) Then
+            FolderBrowserDialog1.SelectedPath = txtOutputDir.Text
+            If FolderBrowserDialog1.ShowDialog = DialogResult.OK Then
+                txtOutputDir.Text = FolderBrowserDialog1.SelectedPath
+            End If
+        End If
+    End Sub
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        If Not Directory.Exists(txtOutputDir.Text) Then
+            MessageBox.Show("The specified output directory does not exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
+
+        Dim render_bars As Integer = 5
+        Dim save_bars As Integer = 1
+
+        Dim multiplier As Integer = render_bars
+
+        Dim args As String = "-vbgui "
+        If chk0ToStart.Checked Then
+            args += " -0L"
+            multiplier += save_bars
+        End If
+        If chkStartToEnd.Checked Then
+            args += " -LE"
+            multiplier += save_bars
+        End If
+        If chk0ToEnd.Checked Then
+            args += " -0E"
+            multiplier += save_bars
+        End If
+
+        ProgressBar1.Value = 0
+        ProgressBar1.Maximum = ListView1.Items.Count * multiplier
+
+        pnlReady.Visible = False
+        pnlInProgress.Visible = True
+
+        Dim paths As New List(Of String)
+        For Each item As ListViewItem In ListView1.Items
+            paths.Add(item.Text)
+        Next
+
+        Dim startInfo As New ProcessStartInfo("exportloop.exe", args)
+        startInfo.RedirectStandardInput = True
+        startInfo.RedirectStandardOutput = True
+        startInfo.RedirectStandardError = True
+        startInfo.ErrorDialog = True
+        startInfo.UseShellExecute = False
+        startInfo.CreateNoWindow = True
+        startInfo.WorkingDirectory = txtOutputDir.Text
+
+        currentProcess = Process.Start(startInfo)
+        Dim inp As StreamWriter = currentProcess.StandardInput
+        Dim out As StreamReader = currentProcess.StandardOutput
+        Dim err As StreamReader = currentProcess.StandardError
+        currentProcess.EnableRaisingEvents = True
+
+        AddHandler currentProcess.Exited, AddressOf ProcessFinished
+
+        Dim t As New Task(
+            Sub()
+                While Not out.EndOfStream
+                    Dim text = out.ReadLine
+                    Console.WriteLine(text)
+                    Me.BeginInvoke(New Action(
+                                   Sub()
+                                       lblCurrentFile.Text = text
+                                       If text.StartsWith("vbgui: ") Then
+                                           If text.StartsWith("vbgui: rendering") Then
+                                               ProgressBar1.Value += render_bars
+                                           ElseIf text.StartsWith("vbgui: saving") Then
+                                               ProgressBar1.Value += save_bars
+                                           ElseIf text.StartsWith("vbgui: skipped") Then
+                                               ProgressBar1.Value += multiplier
+                                           ElseIf text.StartsWith("vbgui: finished ") Then
+                                               Dim path = text.Substring("vbgui: finished ".Length)
+                                               For Each item As ListViewItem In ListView1.Items
+                                                   If item.Text = path Then
+                                                       ListView1.Items.Remove(item)
+                                                       Exit For
+                                                   End If
+                                               Next
+                                           End If
+                                       Else
+                                           lblCurrentFile.Text = text
+                                       End If
+                                   End Sub))
+                End While
+            End Sub)
+        t.Start()
+        Dim t2 As New Task(
+            Sub()
+                While Not err.EndOfStream
+                    Dim text = err.ReadLine
+                    Console.Error.WriteLine(text)
+                    MessageBox.Show(text)
+                End While
+            End Sub)
+        t2.Start()
+
+        For Each path As String In paths
+            inp.WriteLine(path)
+        Next
+        inp.WriteLine()
+    End Sub
+
+    Private Sub ProcessFinished(sender As Object, e As EventArgs)
+        Dim p As Process = CType(sender, Process)
+        Me.Invoke(Sub()
+                      If currentProcess.ExitCode = 0 Then
+                          ListView1.Items.Clear()
+                      End If
+                      pnlInProgress.Visible = False
+                      pnlReady.Visible = True
+                      currentProcess = Nothing
+                  End Sub)
+    End Sub
+
+    Private Sub btnOpenFolder_Click(sender As Object, e As EventArgs) Handles btnOpenFolder.Click
+        If Not Directory.Exists(txtOutputDir.Text) Then
+            MessageBox.Show("The specified output directory does not exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
+
+        Process.Start(txtOutputDir.Text)
+    End Sub
+
+    Private Sub btnAbout_Click(sender As Object, e As EventArgs) Handles btnAbout.Click
+        Dim N = Environment.NewLine
+        MessageBox.Show("Export Loop VB - version 1.1.1" & N & "© 2015 libertyernie" & N & N & "https://github.com/libertyernie/vgmstream" & N & N &
+                        "Permission to use, copy, modify, and distribute this software for any " &
+                        "purpose with or without fee is hereby granted, provided that the above " &
+                        "copyright notice and this permission notice appear in all copies." & vbCrLf &
+                        vbCrLf &
+                        "THE SOFTWARE IS PROVIDED ""AS IS"" AND THE AUTHOR DISCLAIMS ALL WARRANTIES " &
+                        "WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF " &
+                        "MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR " &
+                        "ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES " &
+                        "WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN " &
+                        "ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF " &
+                        "OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.")
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        If currentProcess IsNot Nothing Then
+            currentProcess.Kill()
+        End If
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        If currentProcess IsNot Nothing Then
+            currentProcess.Kill()
+        End If
+    End Sub
+End Class
---
+++ exportloop/exportloop.c
@@ -0,0 +1,240 @@
+#include <stdio.h>
+#include <limits.h>
+
+#include "../src/vgmstream.h"
+#include "../src/util.h"
+
+char* remove_extension_and_path(const char* input) {
+	const char* ptr;
+	
+	// Loop through characters backwards to find extension
+	const char* end = input + strlen(input);
+	for (ptr = end; ptr >= input; ptr--) {
+		if (*ptr == '/' || *ptr == '\\') {
+			// No extension
+			break;
+		} else if (*ptr == '.') {
+			// Extension found - remove
+			end = ptr;
+			break;
+		}
+	}
+
+	// Loop through characters backwards to find slash/backslash
+	const char* start = input;
+	for (ptr = end; ptr >= input; ptr--) {
+		if (*ptr == '/' || *ptr == '\\') {
+			// Found directory separator
+			start = ptr + 1;
+			break;
+		}
+	}
+
+	int len = end - start;
+	char* buffer = (char*)malloc(len + 1);
+	strncpy(buffer, start, len);
+	buffer[len] = '\0';
+
+	return buffer;
+}
+
+int usage(int retval, char* name) {
+	fprintf(stderr, "exportloop 1.1.2\n"
+		"https://github.com/libertyernie/vgmstream\n"
+		"\n"
+		"Permission to use, copy, modify, and distribute this software for any " 
+		"purpose with or without fee is hereby granted, provided that the above " 
+		"copyright notice and this permission notice appear in all copies.\n"
+		"\n"
+		"THE SOFTWARE IS PROVIDED ""AS IS"" AND THE AUTHOR DISCLAIMS ALL WARRANTIES " 
+		"WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF " 
+		"MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR " 
+		"ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES " 
+		"WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN " 
+		"ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF " 
+		"OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.\n"
+		"\n"
+		"Usage:\n"
+		"exportloop [options] {files ...}\n"
+		"\n"
+		"Options:\n"
+		"  -vbgui   send extra data to stdout so Export Loop VB can follow the progress\n"
+		"  -0L      export a .WAV file going from 0 to the loop start\n"
+		"  -LE      export a .WAV file going from the loop start to the end\n"
+		"  -0E      export a .WAV file going from 0 to the end\n"
+		"           (will be assumed if neither -0L nor -LE are given)\n"
+		"\n"
+		"If no filenames are given, but there is at least one argument, filenames will be read from standard input.\n"
+		"\n");
+	return retval;
+}
+
+typedef enum {
+	ZERO_TO_LOOP = 1,
+	LOOP_TO_END = 2,
+	ZERO_TO_END = 4
+} export_types;
+
+int exportvgmstream(export_types export_type, int vbgui, const char* infile) {
+	VGMSTREAM* vgmstream = init_vgmstream(infile);
+	if (vgmstream == NULL) {
+		fprintf(stderr, "Could not open file [%s]\n", infile);
+		fflush(stderr);
+		printf("vbgui: skipped\n");
+		return 0;
+	} else {
+		char* infile_noext = remove_extension_and_path(infile);
+
+		if (vbgui) printf("vbgui: rendering\n");
+
+		printf("Rendering: %s\n", infile_noext);
+		printf("Channels: %d\n", vgmstream->channels);
+		fflush(stdout);
+		sample* samples = (sample*)malloc(sizeof(sample) * vgmstream->channels * vgmstream->num_samples);
+		render_vgmstream(samples, vgmstream->num_samples, vgmstream);
+
+		swap_samples_le(samples, vgmstream->channels * vgmstream->num_samples);
+
+		uint8_t* wavbuffer = (uint8_t*)malloc(0x2C);
+		char* outfile = (char*)malloc(strlen(infile_noext) + 20);
+
+		if (export_type & ZERO_TO_LOOP) {
+			if (vbgui) printf("vbgui: saving\n");
+
+			sprintf(outfile, "%s (beginning).wav", infile_noext);
+			printf("Output: %s\n", outfile);
+			fflush(stdout);
+
+			FILE* f = fopen(outfile, "wb");
+			make_wav_header(wavbuffer,
+				vgmstream->loop_start_sample,
+				vgmstream->sample_rate,
+				vgmstream->channels);
+			fwrite(wavbuffer, 0x2C, 1, f);
+			fwrite(samples,
+				sizeof(sample),
+				vgmstream->loop_start_sample * vgmstream->channels,
+				f);
+			fclose(f);
+		}
+		if (export_type & LOOP_TO_END) {
+			if (vbgui) printf("vbgui: saving\n");
+
+			sprintf(outfile, "%s (loop).wav", infile_noext);
+			printf("Output: %s\n", outfile);
+			fflush(stdout);
+
+			FILE* f = fopen(outfile, "wb");
+			make_wav_header(wavbuffer,
+				vgmstream->loop_end_sample - vgmstream->loop_start_sample,
+				vgmstream->sample_rate,
+				vgmstream->channels);
+			fwrite(wavbuffer, 0x2C, 1, f);
+			fwrite(samples + vgmstream->loop_start_sample * vgmstream->channels,
+				sizeof(sample),
+				(vgmstream->loop_end_sample - vgmstream->loop_start_sample) * vgmstream->channels,
+				f);
+			fclose(f);
+		}
+		if (export_type & ZERO_TO_END) {
+			if (vbgui) printf("vbgui: saving\n");
+
+			sprintf(outfile, "%s.wav", infile_noext);
+			printf("Output: %s\n", outfile);
+			fflush(stdout);
+
+			FILE* f = fopen(outfile, "wb");
+			make_wav_header(wavbuffer,
+				vgmstream->num_samples,
+				vgmstream->sample_rate,
+				vgmstream->channels);
+			fwrite(wavbuffer, 0x2C, 1, f);
+			fwrite(samples,
+				sizeof(sample),
+				vgmstream->num_samples * vgmstream->channels,
+				f);
+			fclose(f);
+		}
+
+		free(samples);
+		free(outfile);
+		free(wavbuffer);
+		free(infile_noext);
+
+		if (vbgui) printf("vbgui: finished %s\n", infile);
+
+		close_vgmstream(vgmstream);
+		return 1;
+	}
+}
+
+int main(int argc, char** argv) {
+	if (argc == 1) {
+		return usage(1, argv[0]);
+	}
+
+	export_types export_type = 0;
+	int vbgui = 0;
+
+	int i;
+	for (i = 1; i < argc; i++) {
+		char* arg = argv[i];
+		if (arg == NULL) {
+			// End of parameters
+			break;
+		} else if (strcasecmp("-0L", arg) == 0) {
+			export_type |= ZERO_TO_LOOP;
+		} else if (strcasecmp("-LE", arg) == 0) {
+			export_type |= LOOP_TO_END;
+		} else if (strcasecmp("-0E", arg) == 0) {
+			export_type |= ZERO_TO_END;
+		} else if (strcasecmp("-vbgui", arg) == 0) {
+			vbgui = 1;
+		} else if (strcasecmp("--help", arg) == 0 || strcasecmp("/?", arg) == 0) {
+			return usage(0, argv[0]);
+		} else {
+			// End of loop parameters
+			break;
+		}
+	}
+
+	if (export_type == 0) {
+		export_type = ZERO_TO_END;
+	}
+
+	int filename_count = argc - i;
+	char** filenames = argv + i;
+
+	int errors = 0;
+
+	if (filename_count > 0) {
+		int j;
+		for (j = 0; j < filename_count; j++) {
+			char* infile = filenames[j];
+
+			int result = exportvgmstream(export_type, vbgui, infile);
+			if (!result) errors++;
+		}
+	} else {
+		char buffer[1024];
+		do {
+			fgets(buffer, 1024, stdin);
+			char* ptr;
+			for (ptr = buffer; *ptr != '\0'; ptr++) {
+				if (*ptr == '\0') break;
+				if (*ptr == '\r' || *ptr == '\n') {
+					*ptr = '\0';
+					break;
+				}
+			}
+			if (strlen(buffer) == 0) break;
+
+			int result = exportvgmstream(export_type, vbgui, buffer);
+			if (!result) errors++;
+		} while (1);
+	}
+
+	return errors > 0
+		? 1
+		: 0;
+}
