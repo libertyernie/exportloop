33f29eff87f7bea93c640580814ccec7d438abca
libertyernie <maybeway36@gmail.com>
2015-08-30 14:57:42 -0500

    Well, I don't need this function anymore

--- vgmstream_to_wav/vgmstream_to_wav.c
+++ vgmstream_to_wav/vgmstream_to_wav.c
@@ -28,99 +28,6 @@ int usage(int retval, char* name) {
 	return retval;
 }
 
-// make a header for PCM .wav, with smpl chunk included for looping information. 0x70.
-void make_looping_wav_header(uint8_t * buf, int32_t loop_start, int32_t sample_count, int32_t sample_rate, int channels) {
-	size_t bytecount;
-
-	bytecount = sample_count*channels*sizeof(sample);
-
-	/* RIFF header */
-	memcpy(buf + 0, "RIFF", 4);
-	/* size of RIFF */
-	put_32bitLE(buf + 4, (int32_t)(bytecount + 0x2c - 8 + 0x44));
-
-	/* WAVE header */
-	memcpy(buf + 8, "WAVE", 4);
-
-	/* WAVE fmt chunk */
-	memcpy(buf + 0xc, "fmt ", 4);
-	/* size of WAVE fmt chunk */
-	put_32bitLE(buf + 0x10, 0x10);
-
-	/* compression code 1=PCM */
-	put_16bitLE(buf + 0x14, 1);
-
-	/* channel count */
-	put_16bitLE(buf + 0x16, channels);
-
-	/* sample rate */
-	put_32bitLE(buf + 0x18, sample_rate);
-
-	/* bytes per second */
-	put_32bitLE(buf + 0x1c, sample_rate*channels*sizeof(sample));
-
-	/* block align */
-	put_16bitLE(buf + 0x20, (int16_t)(channels*sizeof(sample)));
-
-	/* significant bits per sample */
-	put_16bitLE(buf + 0x22, sizeof(sample) * 8);
-
-	/* WAVE smpl chunk */
-	memcpy(buf + 0x24, "smpl", 4);
-	/* size of WAVE smpl chunk */
-	put_32bitLE(buf + 0x28, 0x3c);
-
-	/* manufacturer */
-	put_32bitLE(buf + 0x2c, 0);
-
-	/* product */
-	put_32bitLE(buf + 0x30, 0);
-
-	/* sample period */
-	put_32bitLE(buf + 0x34, (int32_t)(1 / (sample_rate * 1000000000)));
-
-	/* MIDI note */
-	put_32bitLE(buf + 0x38, 0);
-
-	/* MIDI pitch fraction */
-	put_32bitLE(buf + 0x3c, 0);
-
-	/* SMPTE format */
-	put_32bitLE(buf + 0x40, 0);
-
-	/* SMPTE offset */
-	put_32bitLE(buf + 0x44, 0);
-
-	/* Number of sample loops */
-	put_32bitLE(buf + 0x48, 1);
-
-	/* Number of optional fields after sample loops */
-	put_32bitLE(buf + 0x4c, 0);
-
-	/* Unique identifier for loop */
-	put_32bitLE(buf + 0x50, 1);
-
-	/* Loop type */
-	put_32bitLE(buf + 0x54, 0);
-
-	/* Loop start */
-	put_32bitLE(buf + 0x58, loop_start);
-
-	/* Loop end */
-	put_32bitLE(buf + 0x5c, sample_count);
-
-	/* Loop fractional area */
-	put_32bitLE(buf + 0x60, 0);
-
-	/* Loop play count */
-	put_32bitLE(buf + 0x64, 0);
-
-	/* WAVE data chunk */
-	memcpy(buf + 0x68, "data", 4);
-	/* size of WAVE data chunk */
-	put_32bitLE(buf + 0x6c, (int32_t)bytecount);
-}
-
 int main(int argc, char** argv) {
 	if (argc != 2) {
 		return usage(1, argv[0]);
@@ -139,11 +46,20 @@ int main(int argc, char** argv) {
 		swap_samples_le(samples, vgmstream->channels * vgmstream->num_samples);
 
 		if (vgmstream->loop_flag) {
-			uint8_t header[0x70];
-			make_looping_wav_header(&header, vgmstream->loop_start_sample, vgmstream->num_samples, vgmstream->sample_rate, vgmstream->channels);
+			uint8_t header[0x2c];
+			make_wav_header(&header, vgmstream->num_samples, vgmstream->sample_rate, vgmstream->channels);
+
+			uint32_t size = get_32bitLE(header + 4);
+			size += 0x44;
+			put_32bitLE(header + 4, size);
+
+			uint8_t smpl[0x44];
+			make_smpl_chunk(&smpl, vgmstream->loop_start_sample, vgmstream->loop_end_sample);
+
 			_setmode(fileno(stdout), O_BINARY);
-			fwrite(header, 0x70, 1, stdout);
+			fwrite(header, 0x2c, 1, stdout);
 			fwrite(samples, sizeof(sample), vgmstream->channels * vgmstream->num_samples, stdout);
+			fwrite(smpl, 0x44, 1, stdout);
 			fflush(stdout);
 		}
 
